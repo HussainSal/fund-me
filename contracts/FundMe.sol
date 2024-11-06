914357fb496d4de8812f421037674207a3e1bad1
HussainSal <salmanmian54321@gmail.com>
2024-11-06 08:22:53 +0530

    feat :- inprogress - using aggv3interface to get USD info

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
+import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
      uint256 minimumUsd = 50;
 
@@ -10,4 +12,18 @@ contract FundMe {
 
         require(msg.value >= minimumUsd, "Atleast 50 USD are required for the transaction.");
      }
+
+     function getPrice () public view {
+        // ABI
+        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound ) = priceFeed.latestRoundData();
+        
+     }
+
+     function getConversion () public {
+
+     }
+
 }
